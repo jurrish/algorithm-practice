6a177d927b23487706dd98a8f9a3c22270920a37
Frank Iriarte <jurrish@gmail.com>
2017-04-15 16:53:45 -0700

    must finish up linked-list stuff and bst

--- linked-list/linked-list.js
+++ linked-list/linked-list.js
@@ -1,19 +1,22 @@
 //node, each node has a next. head only refers to next til it points to null
 
 
-//start with a node
-let node = function(val){
+//think of this like a phone list in your phone. I want to add/delete people from the list of people inside the memory container that can flex depending on inputs. But, I only have a reference to where i'm at, and where i'm going
+
+//start with a node / person
+const Node = function(val){
   this.value = val;
   this.next = null;
 };
 
-// then create a constructor function
+// then create a constructor function that creates the phone list and takes in an array of people
 const SinglyLinkedList = function(arr){
   arr.forEach(val => {
     this.append(val);
   });
 };
 
+//prepend a value into the list of people
 SinglyLinkedList.prototype.prepend = function(val){
   let node = new Node(val);
 
@@ -26,3 +29,17 @@ SinglyLinkedList.prototype.prepend = function(val){
   this.head = node;
   return node.val;
 };
+
+//append a value into the list of people
+SinglyLinkedList.prototype.append = function(val){
+  let node = new Node(val);
+  let lastNode = null;
+
+  if(!this.head){
+    this.head = node;
+    return node.val;
+  }
+
+
+
+}
