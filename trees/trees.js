daad59c69426904cfe197cf0e0d1f1508dbc9ea5
Frank Iriarte <jurrish@gmail.com>
2017-05-24 13:17:17 -0700

    treeeeee starts

---
+++ trees/trees.js
@@ -0,0 +1,13 @@
+'use strict';
+
+//trees have one root
+//a root is a node
+//a node has a value prop and children array
+
+let root = {
+  value: null,
+  children: [],
+};
+
+
+console.log('hello world');
