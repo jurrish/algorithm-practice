6e06618e2f2dc1885bd93e7d03e2b15feb18e24c
Frank Iriarte <jurrish@gmail.com>
2017-05-26 11:46:12 -0700

    recursion practice example

--- trees/more-trees.js
+++ trees/more-trees.js
@@ -23,6 +23,18 @@ function Tree(data){
 //Tree creates a new instance of node with the data that's passed in
 //Tree also sets that node to it's _root
 //so, if we create an instance of tree with some value, it automatically creates a new node instance
-
+//this sets up our heirarcy because of the properties that live in the constructor of NODE as well as TREE
 let tree = new Tree('CEO');
 console.log(tree);
+
+//recursion refresher
+//recursion is when a function calls itself until it doesnt
+//set base case first!
+let countDownFrom = (num) => {
+  console.log(num);
+  if(num === 0) return;
+  countDownFrom(num - 1);
+};
+countDownFrom(10);
+//should output :
+//10, 9,8,7...
